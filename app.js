e7d652e7c6bd065efddb1def7cbbb6d778cf34b5
midhat Ademovic <midhatademovic@hotmail.com>
2024-04-21 16:19:22 +0200

    finishpart

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const table= document.getElementById('#table');
-const field = document.getElementsByClassName('field');
+const fields = document.getElementsByClassName('field');
+const restartBtn = document.getElementById('restartBtn');
 const players=['X, O'];
 let currentPlayer= players[0];
 
 
-const winnCombinations = [
+const winCombinations = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -13,4 +14,35 @@ const winnCombinations = [
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6]
-]
+];
+
+for (let i = 0; i < fields.length; i++) {
+    fields[i].addEventListener('click', function() {
+        if (!this.innerHTML) {
+            this.innerHTML = currentPlayer;
+            checkWinner(); 
+            currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
+        }
+    });
+}
+
+function checkWinner() {
+    for (let combination of winCombinations) {
+        const [a, b, c] = combination;
+        if (fields[a].innerHTML && fields[a].innerHTML === fields[b].innerHTML && fields[a].innerHTML === fields[c].innerHTML) {
+            alert(`${fields[a].innerHTML} wins!`);
+            resetGame();
+            return;
+        }
+    }
+};
+
+
+function resetGame() {
+    for (let i = 0; i < fields.length; i++) {
+        fields[i].innerHTML = '';
+    }
+    currentPlayer = players[0]; 
+}
+
+restartBtn.addEventListener('click', resetGame);
